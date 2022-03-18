8bb5bbe91db92237c96a84c557c94a3383f9222c
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-03-18 17:13:06 +0300

    Update Pigeon.java

--- src/com/muwaffaq/innopolis/solid/LSP_IS/wrong/Pigeon.java
+++ src/com/muwaffaq/innopolis/solid/LSP_IS/wrong/Pigeon.java
@@ -17,10 +17,4 @@ public class Pigeon implements Bird {
     public void hatch() {
         System.out.println("laying eggs");
     }
-    /// ❌ ❌ ❌ ❌ violate Liskov Principle
-    /// Pigeon can't swim
-    @Override
-    public void swim() {
-        System.out.println("I can't swim");
-    }
 }
